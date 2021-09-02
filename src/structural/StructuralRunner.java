198f1a7a69aec54f4ffe08be88f2e7bc8a19c09f
Evgen <evgenij017@gmail.com>
2021-09-02 15:27:49 +0300

    Ad a Structural Patterns pacage with Facade pattern

---
+++ src/structural/StructuralRunner.java
@@ -0,0 +1,15 @@
+package structural;
+
+public class StructuralRunner {
+    public static void main(String[] args) {
+
+        System.out.println("--FACADE--");
+
+        // Facade //
+        Facade facade = new Facade();
+        facade.allertsOn();
+        facade.allertsOff();
+        //////////////////////////////
+
+    }
+}
